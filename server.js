ea3e31b0da3086397dfe04e131604d17e7085e53
kim-genius <138184932+kim-genius@users.noreply.github.com>
2023-11-26 22:27:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGY3MzVkMjg1ODQ3NWU4MGFiMTMzODk1Y2VlZTlkMyIsInN1YiI6IjY0ZjMxNDViNzdkMjNiMDBjYjg3MTA1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z-E0Cizv8nDJJccSkhgLqH0Fy9V_FxCLHaLvFz_m9Jk'
+        Authorization: ''
       }
     };
 
@@ -41,7 +41,7 @@ app.post('/api/movie',(req,res)=>{
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGY3MzVkMjg1ODQ3NWU4MGFiMTMzODk1Y2VlZTlkMyIsInN1YiI6IjY0ZjMxNDViNzdkMjNiMDBjYjg3MTA1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z-E0Cizv8nDJJccSkhgLqH0Fy9V_FxCLHaLvFz_m9Jk'
+        Authorization: ''
       } 
     };
 
@@ -60,7 +60,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGY3MzVkMjg1ODQ3NWU4MGFiMTMzODk1Y2VlZTlkMyIsInN1YiI6IjY0ZjMxNDViNzdkMjNiMDBjYjg3MTA1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z-E0Cizv8nDJJccSkhgLqH0Fy9V_FxCLHaLvFz_m9Jk'
+    Authorization: ''
   }
 };
 
@@ -88,7 +88,7 @@ const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGY3MzVkMjg1ODQ3NWU4MGFiMTMzODk1Y2VlZTlkMyIsInN1YiI6IjY0ZjMxNDViNzdkMjNiMDBjYjg3MTA1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z-E0Cizv8nDJJccSkhgLqH0Fy9V_FxCLHaLvFz_m9Jk'
+    Authorization: ''
   }
 };
 
